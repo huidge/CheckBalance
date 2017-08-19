a6f1d710c59569254ec1d88c0962dff472bff61e
huidge <919900755@qq.com>
2017-08-19 17:34:27 +0800

    2017-8-19:初始化

---
+++ js/BaiduStatistics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?5d487c179f27c78f474da9ad8e49a83c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
---
+++ js/core.js
@@ -0,0 +1,5 @@
+function print(){  
+   var queryString="http://weixin.17wanxiao.com/mobile/index.html?customerCode=1007&type=3&outid=";
+   var sno=myform.name.value;
+  queryString=queryString+sno+"&openid=o-wzYjnJWu6kv2BAIDCbE0HjWkTM&name=%E9%83%AD%E5%BF%97%E8%BE%89";
+  window.open(queryString,"","");  }  
